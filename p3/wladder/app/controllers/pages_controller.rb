2b6136f0307f0a62cde5444d14db91a7aeed6411
Ryan Nelson <rnelson20@mail.csuchico.edu>
2014-10-09 05:25:25 +0000

    First working version of this app

--- p3/wladder/app/controllers/pages_controller.rb
+++ p3/wladder/app/controllers/pages_controller.rb
@@ -1,6 +1,46 @@
 class PagesController < ApplicationController
 load "#{Rails.root}/lib/ladder.rb"
+
 def home
-	@greeting = "Home action says: Hello world!"	
- end
+	@page_header = "CSCI465 P3: Word Ladder"
+	@by_line = "I made this! (Ryan Nelson)"	
+	@start_words = dictionary.sample(2)
+end
+def shazam
+	@start_word = params[:start_word]
+	@step1 = params[:step1] 
+	@step2 = params[:step2] 
+	@step3 = params[:step3] 
+	@step4 = params[:step4] 
+	@step5 = params[:step5] 
+	@goal = params[:goal]
+
+	@good = false
+	@winning_message = "Sorry you failed!"
+
+	filled_words = Array.new
+	filled_words.push(@start_word)
+
+	if !@step1.empty?
+		filled_words.push(@step1)
+	end
+	if !@step2.empty?
+		filled_words.push(@step2)
+	end
+	if !@step3.empty?
+		filled_words.push(@step3)
+	end
+	if !@step4.empty?
+		filled_words.push(@step4)
+	end
+	if !@step5.empty?
+		filled_words.push(@step5)
+	end
+	filled_words.push(@goal)
+
+	@good = legal filled_words
+	if @good
+		@winning_message = "You have won!"
+	end
+end
 end
