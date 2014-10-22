8defc09fb46c9ba48789aeaba3a316aec21b086e
Ryan Nelson <rnelson20@mail.csuchico.edu>
2014-10-22 07:31:20 +0000

    Stopping here have some more base stuff

--- doi/app/controllers/pages_controller.rb
+++ doi/app/controllers/pages_controller.rb
@@ -3,6 +3,25 @@ class PagesController < ApplicationController
 def home
         @page_header = "CSCI465 P4: DOI Server"
         @by_line = "I made this! (Ryan Nelson)"
+	@doi_bases = DoiBase.all
+	@doi_history = DoiHistory.all
+end
+
+def show
+	@doi_base = DoiBase.find(params[:id])
+end
+
+def new
+	@doi_base = DoiBase.new
+end
+
+def create
+	@doi_base = DoiBase.new(params.require(:doi_base).permit(:doi_path, :created))
+	if @doi_base.save then
+		redirect_to @index, notice: 'The new DOI was created'
+	else
+		render :new
+	end
 end
 
 end
--- doi/config/routes.rb
+++ doi/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+ get 'pages/new', to: 'pages#new', as: 'new_doi_base'
+ post 'pages', to: 'pages#create'
  root to: 'pages#home'
 end
